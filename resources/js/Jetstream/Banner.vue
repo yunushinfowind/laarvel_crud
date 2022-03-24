c9ff8f8d38570576bced3baa0976c35125d35c18
yunushinfowind <yunush.infowind@gmail.com>
2022-03-24 19:58:02 +0530

    chnages

--- resources/js/Jetstream/Banner.vue
+++ resources/js/Jetstream/Banner.vue
@@ -5,13 +5,19 @@
                 <div class="flex items-center justify-between flex-wrap">
                     <div class="w-0 flex-1 flex items-center min-w-0">
                         <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
-                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="style == 'success'">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
+                            <img
+                                width="89"
+                                height="50"
+                                src="/images/C-logo.png"
+                                alt="Logo"
+                            />
 
-                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="style == 'danger'">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                            </svg>
+                            <img
+                                width="89"
+                                height="50"
+                                src="/images/C-logo.png"
+                                alt="Logo"
+                            />
                         </span>
 
                         <p class="ml-3 font-medium text-sm text-white truncate">
@@ -26,9 +32,12 @@
                             :class="{ 'hover:bg-indigo-600 focus:bg-indigo-600': style == 'success', 'hover:bg-red-600 focus:bg-red-600': style == 'danger' }"
                             aria-label="Dismiss"
                             @click.prevent="show = false">
-                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+                            <img
+                                width="89"
+                                height="50"
+                                src="/images/C-logo.png"
+                                alt="Logo"
+                            />
                         </button>
                     </div>
                 </div>
